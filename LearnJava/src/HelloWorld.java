7897d0b3b9661e71f4da16235a518e98560f19fe
Jerry Shang <jerryshang@Jerrys-MacBook-Pro.local>
2021-03-10 22:51:54 -0800

    update hello world

--- LearnJava/src/HelloWorld.java
+++ LearnJava/src/HelloWorld.java
@@ -2,5 +2,27 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("I rule this world");
+		int x = 3;
+		String name = "Jerry";
+		x = x * 17;
+		double d = Math.random();
+		// Verify the value x
+		while (x > 12) {
+			x = x - 1;
+		}
+		for (int i = 0; i < 8; i = i + 1) {
+			System.out.println("i is now" + i);
+		}
+		if (x == 10) {
+			System.out.println("x must be 10");
+		}
+		else {
+			System.out.println("x isn't 10");
+		}
+		if ((x < 30) & (name.equals("Jerry"))) {
+			System.out.println("Yes so what?");
+		}
+		System.out.println("great info to master");
 	}
 }
