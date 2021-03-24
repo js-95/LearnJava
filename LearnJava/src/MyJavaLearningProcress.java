6b5b63e7ea9492ecebc6838648eb323b5d6ac149
Jerry Shang <jerryshang@Jerrys-MacBook-Pro.local>
2021-03-23 20:39:21 -0700

    While loop and If statement

---
+++ LearnJava/src/MyJavaLearningProcress.java
@@ -0,0 +1,25 @@
+
+public class MyJavaLearningProcress {
+
+	public static void main (String[] args) {
+		int beerNum = 99;
+		String word = "bottles";
+		while (beerNum > 0) {
+			System.out.println (beerNum + " " + word + "of beer on the wall");
+			System.out.println (beerNum + " " + word + "of beer" );
+			System.out.println ("take one out");
+			System.out.println ("pass it around");
+			beerNum = beerNum - 1;
+		
+			if (beerNum == 1) {
+				word = "bottle";
+			}
+			if (beerNum > 0) {
+				System.out.println (beerNum + " " + word + "of beer on the wall");
+			} else { 
+				System.out.println ("no more bottles on the wall");
+				
+			}			
+		}
+	}
+}
